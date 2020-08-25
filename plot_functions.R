0feea890890482a941c3f5e95109f7669e482fc2
twoseventwo <matthew.hall@bdi.ox.ac.uk>
2020-08-25 18:23:56 +0100

    Added file for plots (potentially a package later)

---
+++ plot_functions.R
@@ -0,0 +1,71 @@
+age.pyramid.plot <- function(aggregated.tbl, ...){
+  
+  # print(nrow(aggregated.tbl))
+  
+  max.count = aggregated.tbl %>% group_by(agegp5, sex) %>% summarise(sac = sum(abs(count))) %>% pull(sac) %>% max()
+  
+  order.of.magnitude <- ceiling(log10(max.count))
+  
+  if(as.numeric(substr(as.character(max.count), 1, 1)) > 5){
+    tick.increment <- 10^(order.of.magnitude-1)
+  } else {
+    tick.increment <- 10^(order.of.magnitude-1)/2
+  }
+  
+  plot.breaks <- seq(-(ceiling(max.count/tick.increment)*tick.increment), ceiling(max.count/tick.increment)*tick.increment, by = tick.increment)
+  plot.labels <- as.character(c(rev(seq(tick.increment, ceiling(max.count/tick.increment)*tick.increment, by = tick.increment)),
+                                0,
+                                seq(tick.increment, ceiling(max.count/tick.increment)*tick.increment, by= tick.increment)))
+  
+  plt <- ggplot() +
+    geom_bar(data = (aggregated.tbl %>% filter(sex == "Male")), aes(x=agegp5, y=-count, fill = outcome), stat = "identity") +
+    geom_bar(data = aggregated.tbl %>% filter(sex == "Female"), aes(x=agegp5, y=count, fill = outcome),  stat = "identity") +
+    coord_flip(clip = 'off') +
+    theme_bw() +
+    scale_fill_brewer(palette = 'Set2', name = "Outcome", drop="F") +
+    xlab("Age group") +
+    ylab("Number of patients") +
+    scale_x_discrete(drop = "F") +
+    scale_y_continuous(
+      # currently in hard-coded increments of 5. @todo make this better
+      breaks = plot.breaks,
+      labels = plot.labels,
+      limits = c(-1.1*max.count, 1.1*max.count)) +
+    annotation_custom(
+      grob = textGrob(label = "Males", hjust = 0.5, gp = gpar(cex = 1.5)),
+      ymin = -max.count/2,
+      ymax = -max.count/2,
+      xmin = length(levels(aggregated.tbl$agegp5))+1.5 ,
+      xmax = length(levels(aggregated.tbl$agegp5))+1.5) +
+    annotation_custom(
+      grob = textGrob(label = "Females", hjust = 0.4, gp = gpar(cex = 1.5)),
+      ymin = max.count/2,
+      ymax = max.count/2,
+      xmin = length(levels(aggregated.tbl$agegp5))+1.5,
+      xmax = length(levels(aggregated.tbl$agegp5))+1.5) +
+    theme(plot.margin=unit(c(30,5,5,5.5,5.5),"pt"),
+          axis.text.x=element_text(angle = -90, vjust = 0.5))
+  
+  plt
+  
+}
+
+
+outcomes.by.admission.date.plot <- function(aggregated.tbl, embargo.limit, ...){
+  
+  peak.cases <- aggregated.tbl %>% group_by(year.epiweek.admit) %>% summarise(count = sum(cum.count)) %>% pull(count) %>% max()
+
+  plt <- ggplot(aggregated.tbl) +
+    geom_col(aes(x = year.epiweek.admit, y=cum.count, fill = outcome), width = 0.95) +
+    theme_bw() +
+    scale_fill_brewer(palette = 'Set2', name = "Outcome", drop="F") +
+    xlab("Epidemiological week of admission/symptom onset") +
+    ylab("Cumulative patient records") +
+    ylim(c(0, 1.05*peak.cases)) +
+    scale_x_discrete(drop = F) +
+    annotate(geom = "text", label = "*", x = length(levels(aggregated.tbl$year.epiweek.admit)),
+             y = peak.cases, size =15) +
+    theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) 
+  
+  return(plt)
+}
