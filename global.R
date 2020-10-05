ddb13a735c9ea8c38ca6796de77433253a6af212
twoseventwo <matthew.hall@bdi.ox.ac.uk>
2020-10-05 10:54:50 +0100

    Added renv library

--- global.R
+++ global.R
@@ -1,8 +1,7 @@
 library(devtools)
-
+library(renv)
 # if this keeps failing, try turning off caching in shinyapps (the web interface). And then deploy _twice_. The first time the browser will open with an error message. That's expected.
 # Then comment out the following line for the second deployment. It's mad, but there you go.
-
 devtools::install_github("ISARICDataPlatform/CovidClinicalDataProcessor")
 library(CovidClinicalDataProcessor)
 library(shinydashboard)
