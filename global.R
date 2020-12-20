b5af7dbbdeb89428bca58949d256ac173287e251
twoseventwo <matthew.hall@bdi.ox.ac.uk>
2020-12-20 13:41:50 +0000

    Got rid of double .Rdata loads, and libraries before .RData loads

--- global.R
+++ global.R
@@ -1,3 +1,64 @@
+
+library(devtools)
+library(renv)
+# if this keeps failing, try turning off caching in shinyapps (the web interface). And then deploy _twice_. The first time the browser will open with an error message. That's expected.
+# Then comment out the following line for the second deployment. It's mad, but there you go.
+# devtools::install_github("ISARICDataPlatform/CovidClinicalDataProcessor")
+# devtools::install_github("kamclean/collaborator")
+library(CovidClinicalDataProcessor)
+library(shinydashboard)
+library(shinyWidgets)
+library(ISOcodes)
+library(tidyverse)
+library(dtplyr)
+library(grid)
+library(lubridate)
+library(glue)
+library(data.table)
+library(tidyfast)
+library(ggupset)
+library(ggmap)
+library(leaflet)
+library(mapview) 
+# library(collaborator)
+library(knitr)
+library(viridis)  #using scale_fill_viridis
+library(scales)
+library(binom)
+library(DiagrammeR)
+library(magrittr)
+library(flextable)
+library(htmltools)
+
+
+
+
+epiweek.year <- function(date){
+  if(is.na(date)){
+    return(NA)
+  }
+  if(year(date)==2019 & date > ymd("2019-12-28")){
+    2020
+  } else {
+    year(date)
+  }
+}
+
+outcome.remap <- function(oc, od){
+  if(is.na(od) & is.na(oc)){
+    "censored"
+  } else {
+    out <- case_when(is.na(oc) ~ NA_character_,
+                     oc == "Death" ~ "Death",
+                     oc == "Discharged Alive" ~ "Discharge",
+                     oc == "LFTU" ~ "LFTU",
+                     oc == "Ongoing care" ~ "Ongoing care",)
+  }
+}
+
+source("plot_functions.R")
+
+
 #Load all the datasets
 
 base::load("age_pyramid_input.rda")  
@@ -60,61 +121,6 @@ base::load("outcome.age.sex.table.rda")
 
 
 
-library(devtools)
-library(renv)
-# if this keeps failing, try turning off caching in shinyapps (the web interface). And then deploy _twice_. The first time the browser will open with an error message. That's expected.
-# Then comment out the following line for the second deployment. It's mad, but there you go.
-# devtools::install_github("ISARICDataPlatform/CovidClinicalDataProcessor")
-# devtools::install_github("kamclean/collaborator")
-library(CovidClinicalDataProcessor)
-library(shinydashboard)
-library(shinyWidgets)
-library(ISOcodes)
-library(tidyverse)
-library(dtplyr)
-library(grid)
-library(lubridate)
-library(glue)
-library(data.table)
-library(tidyfast)
-library(ggupset)
-library(ggmap)
-library(leaflet)
-library(mapview) 
-# library(collaborator)
-library(knitr)
-library(viridis)  #using scale_fill_viridis
-library(scales)
-library(binom)
-library(DiagrammeR)
-library(magrittr)
-library(flextable)
-library(htmltools)
-
-epiweek.year <- function(date){
-  if(is.na(date)){
-    return(NA)
-  }
-  if(year(date)==2019 & date > ymd("2019-12-28")){
-    2020
-  } else {
-    year(date)
-  }
-}
-
-outcome.remap <- function(oc, od){
-  if(is.na(od) & is.na(oc)){
-    "censored"
-  } else {
-    out <- case_when(is.na(oc) ~ NA_character_,
-                     oc == "Death" ~ "Death",
-                     oc == "Discharged Alive" ~ "Discharge",
-                     oc == "LFTU" ~ "LFTU",
-                     oc == "Ongoing care" ~ "Ongoing care",)
-  }
-}
-
-source("plot_functions.R")
 
 age.bound.lookup <- tibble(slider_agegp10 = cut(1:100, right = FALSE, breaks = c(0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 120)) %>% unique()) %>%
   mutate(lower.age.bound  = map_dbl(slider_agegp10, extract.age.boundaries, TRUE)) %>%
@@ -125,27 +131,15 @@ age.bound.lookup <- tibble(slider_agegp10 = cut(1:100, right = FALSE, breaks = c
 
 age.levels <- age.bound.lookup %>% pull(slider_agegp10) %>% levels
 
-base::load("age_pyramid_input.rda")  
-     
-
-base::load("outcome_admission_date_input.rda")
-
-base::load("symptom_prevalence_input.rda")
-
 rev.symptom.order <- symptom.prevalence.input %>% pull(nice.symptom) %>% unique() %>% sort(decreasing = TRUE)
 
 symptom.prevalence.input <- symptom.prevalence.input %>%
   mutate(nice.symptom = factor(nice.symptom, levels = rev.symptom.order)) 
 
-base::load("comorbidity_prevalence_input.rda")
-
 rev.comorbidity.order <- comorbidity.prevalence.input %>% pull(nice.comorbidity) %>% unique() %>% sort(decreasing = TRUE)
 
 comorbidity.prevalence.input <- comorbidity.prevalence.input %>%
   mutate(nice.comorbidity = factor(nice.comorbidity, levels = rev.comorbidity.order))
-
-
-base::load("treatment_use_proportion_input.rda")
 
 rev.treatment.order <- treatment.use.proportion.input %>% pull(nice.treatment) %>% unique() %>% sort(decreasing = TRUE)
 
@@ -172,13 +166,6 @@ month.options <- c("Dec 2019", glue("{months} {2020}"))
 
 map.data <- read_rds(here::here("map_data.rds"))
 
-
-#################load data for hospital stays
-base::load("length_of_stay_sex_input.rda")
-base::load("length_of_stay_age_input.rda")
-base::load("admission_to_icu_input.rda")
-base::load("status_by_time_after_admission_input.rda")
-base::load("length_of_stay_icu_input.rda")
 
 report_auth <- function(df, name, group = NULL, subdivision = NULL, path = NULL,
                         name_sep = ", ", group_brachet = "()",group_sep = "; "){
@@ -263,54 +250,6 @@ report_auth <- function(df, name, group = NULL, subdivision = NULL, path = NULL,
   return(gsub("\n\n", " ", output$auth_out))}
 
 knit("markdown/Contributor_listmap.Rmd", output = "markdown/Contributor_listmap.md")
-
-
-#Load the vs data
-base::load("data_plot_vs_resp.rda")
-base::load("data_plot_vs_hr.rda")
-base::load("data_plot_vs_temp.rda")
-base::load("data_plot_vs_sysbp.rda")
-base::load("data_plot_vs_oxysat.rda")
-#Load the laboratory data
-base::load("data_plot_lab_crp.rda")
-base::load("data_plot_lab_lym.rda")
-base::load("data_plot_lab_neut.rda")
-base::load("data_plot_lab_wbc.rda")
-base::load("data_plot_lab_urean.rda")
-base::load("data_plot_lab_pt.rda")
-base::load("data_plot_lab_alt.rda")
-base::load("data_plot_lab_aptt.rda")
-base::load("data_plot_lab_ast.rda")
-base::load("data_plot_lab_bili.rda")
-
-#load patient by country data
-base::load("patient_by_country_input.rda")
-
-#load summary & flowchart data
-base::load("summary_input.rda")
-
-#Load the comorbidty by age plots
-base::load("data_plot_comorbid_asthma.rda")
-base::load("data_plot_comorbid_malignant_neoplasm.rda")
-base::load("data_plot_comorbid_obesity.rda")
-base::load("data_plot_comorbid_diabetes.rda")
-base::load("data_plot_comorbid_dementia.rda")
-base::load("data_plot_comorbid_smoking.rda")
-base::load("data_plot_comorbid_hypertension.rda")
-#Load the symptoms by age plots
-base::load("data_plot_symptoms_history_of_fever.rda")
-base::load("data_plot_symptoms_cough.rda")
-base::load("data_plot_symptoms_cough_fever.rda")
-base::load("data_plot_symptoms_shortness_of_breath.rda")
-base::load("data_plot_symptoms_cought_fever_shortness_of_breath.rda")
-base::load("data_plot_symptoms_upper_respiratory_tract_symptoms.rda")
-base::load("data_plot_symptoms_altered_consciousness_confusion.rda")
-base::load("data_plot_symptoms_constitutional.rda")
-base::load("data_plot_symptoms_vomiting_nausea.rda")
-base::load("data_plot_symptoms_diarrhoea.rda")
-base::load("data_plot_symptoms_abdominal_pain.rda")
-#Load the heatmap data
-base::load("data_plot_heatmap.rda")
 
 
 
