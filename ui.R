996828e10d46646013b79dfe1ffcb65578fe8471
jiaweioxford <jiawei1996pku@gmail.com>
2021-03-09 13:21:36 +0000

    Add numbers by group caption

--- ui.R
+++ ui.R
@@ -311,7 +311,7 @@ dashboardPage(
                 ),
                 box(
                   plotOutput("lengthofstayICU", height = "500px"),
-                  "This only includes cases with reported outcomes. The coloured areas indicate the kernel probability density of the observed data and the box plots show the median and interquartile range of the variable of interest. The upper value was truncated at 97.5%.",
+                  "This only includes cases with reported outcomes. The coloured areas indicate the kernel probability density of the observed data and the box plots show the median and interquartile range of the variable of interest. The upper value was truncated at 97.5%. The number below is the total number in each group.",
                   width = 6,
                   height = 600,
                   solidHeader = T,
@@ -337,7 +337,7 @@ dashboardPage(
               fluidRow(
                 box(
                   plotOutput("lengthofstaySex", height = "500px"),
-                  "This only includes cases with reported outcomes. The coloured areas indicate the kernel probability density of the observed data and the box plots show the median and interquartile range of the variable of interest. The upper value was truncated at 97.5%.",
+                  "This only includes cases with reported outcomes. The coloured areas indicate the kernel probability density of the observed data and the box plots show the median and interquartile range of the variable of interest. The upper value was truncated at 97.5%. The number below is the total number in each sex group.",
                   width = 6,
                   height = 620,
                   solidHeader = T,
@@ -345,7 +345,7 @@ dashboardPage(
                 ),
                 box(
                   plotOutput("lengthofstayAge", height = "500px"),
-                  "This only includes cases with reported outcomes. The coloured areas indicate the kernel probability density of the observed data and the box plots show the median and interquartile range of the variable of interest. The upper value was truncated at 97.5%.",
+                  "This only includes cases with reported outcomes. The coloured areas indicate the kernel probability density of the observed data and the box plots show the median and interquartile range of the variable of interest. The upper value was truncated at 97.5%. The number below is the total number in each age group.",
                   width = 6,
                   height = 620,
                   solidHeader = T,
