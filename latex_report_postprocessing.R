1b35f3e420a716ad4a465ac074e30141e8cefd9f
twoseventwo <matthew.hall@bdi.ox.ac.uk>
2021-07-07 18:16:06 +0100

    Fix to figure numbering

--- latex_report_postprocessing.R
+++ latex_report_postprocessing.R
@@ -29,28 +29,12 @@ new.fig5 <- str_replace(new.fig5, fixed("\\newline"), interfigure.5.a)
 new.fig5 <- str_replace(new.fig5, fixed("\\newline"), interfigure.5.b)
 
 tex.lines[fig5.line.no] <- new.fig5
+tex.lines <- append(tex.lines, "\\setcounter{figure}{5}", after = fig5.line.no )
 
 
 
 # Replace figure 6
-fig6.line.no <- which(startsWith(tex.lines, "\\subfloat[Frequency of comorbidities or other concomitant conditions"))
-
-old.fig6 <- tex.lines[fig6.line.no]
-
-fig6.caption.and.label <- str_match(old.fig6, ".*(\\\\caption.*)")[,2]
-
-new.fig6 <- str_replace(old.fig6, fixed(fig6.caption.and.label), "")
-
-interfigure.6 <- paste0(fig6.caption.and.label, "\\end{figure}", "\\begin{figure}\\ContinuedFloat")
-
-new.fig6 <- str_replace(new.fig6, fixed("\\newline"), interfigure.6)
-
-tex.lines[fig6.line.no] <- new.fig6
-
-
-
-# Replace figure 8
-fig8.line.no <- which(startsWith(tex.lines, "\\subfloat[Alanine transaminase (ALT"))
+fig8.line.no <- which(startsWith(tex.lines, "\\subfloat[Frequency of comorbidities or other concomitant conditions"))
 
 old.fig8 <- tex.lines[fig8.line.no]
 
@@ -58,46 +42,69 @@ fig8.caption.and.label <- str_match(old.fig8, ".*(\\\\caption.*)")[,2]
 
 new.fig8 <- str_replace(old.fig8, fixed(fig8.caption.and.label), "")
 
-interfigure.8 <- paste0(fig8.caption.and.label, "\\end{figure}", "\\begin{figure}\\ContinuedFloat\\subfloat[Neutrophils")
+interfigure.8 <- paste0(fig8.caption.and.label, "\\end{figure}", "\\begin{figure}\\ContinuedFloat")
 
-new.fig8 <- str_replace(new.fig8, fixed("\\newline\\subfloat[Neutrophils"), interfigure.8)
+new.fig8 <- str_replace(new.fig8, fixed("\\newline"), interfigure.8)
 
 tex.lines[fig8.line.no] <- new.fig8
+
+tex.lines <- append(tex.lines, "\\setcounter{figure}{8}", after = fig8.line.no )
+
+
+# Replace figure 8
+fig11.line.no <- which(startsWith(tex.lines, "\\subfloat[Alanine transaminase (ALT"))
+
+old.fig11 <- tex.lines[fig11.line.no]
+
+fig11.caption.and.label <- str_match(old.fig11, ".*(\\\\caption.*)")[,2]
+
+new.fig11 <- str_replace(old.fig11, fixed(fig11.caption.and.label), "")
+
+interfigure.11 <- paste0(fig11.caption.and.label, "\\end{figure}", "\\begin{figure}\\ContinuedFloat\\subfloat[Neutrophils")
+
+new.fig11 <- str_replace(new.fig11, fixed("\\newline\\subfloat[Neutrophils"), interfigure.11)
+
+tex.lines[fig11.line.no] <- new.fig11
+
+tex.lines <- append(tex.lines, "\\setcounter{figure}{11}", after = fig11.line.no )
 
 
 
 # Replace figure 9
-fig9.line.no <- which(startsWith(tex.lines, "\\subfloat[Proportions of patients receiving each treatment"))[1]
+fig12.line.no <- which(startsWith(tex.lines, "\\subfloat[Proportions of patients receiving each treatment"))[1]
 
-old.fig9 <- tex.lines[fig9.line.no]
+old.fig12 <- tex.lines[fig12.line.no]
 
-fig9.caption.and.label <- str_match(old.fig9, ".*(\\\\caption.*)")[,2]
+fig12.caption.and.label <- str_match(old.fig12, ".*(\\\\caption.*)")[,2]
 
-new.fig9 <- str_replace(old.fig9, fixed(fig9.caption.and.label), "")
+new.fig12 <- str_replace(old.fig12, fixed(fig12.caption.and.label), "")
 
-interfigure.9 <- paste0(fig9.caption.and.label, "\\end{figure}", "\\begin{figure}\\ContinuedFloat")
+interfigure.12 <- paste0(fig12.caption.and.label, "\\end{figure}", "\\begin{figure}\\ContinuedFloat")
 
-new.fig9 <- str_replace(new.fig9, fixed("\\newline"), interfigure.9)
+new.fig12 <- str_replace(new.fig12, fixed("\\newline"), interfigure.12)
 
-tex.lines[fig9.line.no] <- new.fig9
+tex.lines[fig12.line.no] <- new.fig12
+
+tex.lines <- append(tex.lines, "\\setcounter{figure}{12}", after = fig12.line.no )
 
 
+# Replace figure 13
+fig13.line.no <- which(startsWith(tex.lines, "\\subfloat[Proportions of patients receiving each treatment"))[2]
 
+old.fig13 <- tex.lines[fig13.line.no]
 
-# Replace figure 10
-fig10.line.no <- which(startsWith(tex.lines, "\\subfloat[Proportions of patients receiving each treatment"))[2]
+fig13.caption.and.label <- str_match(old.fig13, ".*(\\\\caption.*)")[,2]
 
-old.fig10 <- tex.lines[fig10.line.no]
+new.fig13 <- str_replace(old.fig13, fixed(fig13.caption.and.label), "")
 
-fig10.caption.and.label <- str_match(old.fig10, ".*(\\\\caption.*)")[,2]
+interfigure.13 <- paste0(fig13.caption.and.label, "\\end{figure}", "\\begin{figure}\\ContinuedFloat")
 
-new.fig10 <- str_replace(old.fig10, fixed(fig10.caption.and.label), "")
+new.fig13 <- str_replace(new.fig13, fixed("\\newline"), interfigure.13)
 
-interfigure.10 <- paste0(fig10.caption.and.label, "\\end{figure}", "\\begin{figure}\\ContinuedFloat")
+tex.lines[fig13.line.no] <- new.fig13
 
-new.fig10 <- str_replace(new.fig10, fixed("\\newline"), interfigure.10)
+tex.lines <- append(tex.lines, "\\setcounter{figure}{13}", after = fig13.line.no )
 
-tex.lines[fig10.line.no] <- new.fig10
 
 
 write_lines(tex.lines, "Static_report_final.tex")
